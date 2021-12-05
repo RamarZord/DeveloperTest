eae491ef6e7030b988adcec3599540d12f24d532
Simon <simonsmith14@btinternet.com>
2021-12-05 21:11:20 +0000

    DeveloperTest.Tests
    A small sample of unit tests written for the DeveloperTest project.

---
+++ DeveloperTest.Tests/Controllers/EncryptPasswordTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using DeveloperTest.Controllers;
+using System;
+
+namespace DeveloperTest.Tests.Controllers
+{
+    /// <summary>
+    /// Test class for hashing passwords
+    /// </summary>
+    [TestFixture]
+    class EncryptPasswordTest
+    {
+        [TestCase(null, typeof(ArgumentException))]
+        public void HashPassword_IfPasswordNull_Fails(string password, Type expectedException)
+        {
+            Assert.Throws(expectedException, () => EncryptPassword.HashPassword(password));
+        }
+
+        [TestCase("", typeof(ArgumentException))]         
+        public void HashPassword_IfPasswordEmpty_Fails(string password, Type expectedException)
+        {
+            Assert.Throws(expectedException, () => EncryptPassword.HashPassword(password));
+
+        }
+
+        [Test]
+        public void HashPassword_Success()
+        {
+            string expected = "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=";
+            string result = EncryptPassword.HashPassword("password");
+            Assert.AreEqual(result, expected);
+        }
+    }    
+}
---
+++ DeveloperTest.Tests/Controllers/UserDataControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using DeveloperTest.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+
+namespace DeveloperTest.Tests.Controllers
+{
+    [TestClass]
+    public class UserDataControllerTests
+    {
+        [TestMethod]
+        public void AddUserInitialise()
+        {
+            // Arrange
+            UserDataController controller = new UserDataController();
+
+            // Act
+            ViewResult result = controller.AddUser() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+ 
+    }
+}
